ee72992f3a2a374f8a6e111c1d21e984f28741a1
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-09-26 23:13:12 +0100

    Create AuthController.js

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,25 @@
+const User = require("../Models/UserModel");
+const { createSecretToken } = require("../util/Secrets");
+const bcrypt = require("bcryptjs");
+
+module.exports.Signup = async (req, res, next) => {
+  try {
+    const { email, password, createdAt } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, password, createdAt });
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};

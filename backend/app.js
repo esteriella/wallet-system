23437c495e35ea8db6ecb7bec041a549fc0acdcc
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-09-30 16:28:32 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,13 +28,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://estie-wallet-system.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://estie-wallet-system.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+//   })
+// );
+app.use(cors());
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));

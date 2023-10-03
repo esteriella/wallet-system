619205ec9fc388755c503ab012f2f7ff86ed512e
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-10-03 18:49:01 +0100

    Update walletRoutes.js

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -3,7 +3,7 @@ const userVerification = require("../middlewares/authentication");
 const walletRouter = require("express").Router();
 const { verifyToken, verifyUser } = require('../middlewares/authentication');
 
-walletRouter.get('/details/:userId', verifyToken, verifyUser, getWallet);
+walletRouter.get('/balance/:userId', verifyToken, verifyUser, getWallet);
 walletRouter.put('/fund/:userId',  verifyToken, verifyUser, fundWallet);
 
 module.exports = walletRouter;

e7a530781cef0dfe900d6090660092cdf67ad9bc
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-10-03 21:43:57 +0100

    Update authentication.js

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -5,7 +5,7 @@ const { createError } = require("./errors.js");
 
 const verifyToken = (req, res, next) => {
   const bearerToken =  req.headers.authorization?.split(' ')[1];
-  const token = req.cookies.token || bearerToken;
+  const token = bearerToken || req.cookies.token;
   console.log(token)
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
@@ -20,7 +20,7 @@ const verifyToken = (req, res, next) => {
 
 const verifyLogin = (req, res, next) => {
   const bearerToken =  req.headers.authorization?.split(' ')[1];
-  const token = req.cookies.token || bearerToken;
+  const token = bearerToken || req.cookies.token;
   console.log(token)
 
   if (token) {

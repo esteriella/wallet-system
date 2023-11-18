36fe9c9d64acb90c048509a69fb634b01fe73222
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-11-18 20:57:02 +0100

    Update

--- frontend/src/components/bvn/BVNForm.js
+++ frontend/src/components/bvn/BVNForm.js
@@ -1,7 +1,7 @@
 // BVNForm.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -17,23 +17,24 @@ function BVNForm() {
     // formData.append('bvn', data.bvn);
     // formData.append('image', data.image);
 
-    const response = await axios.put(`${api}/verify-bvn/${userId}`, 
-    {
+    const response = await axios.put(
+      `${api}/verify-bvn/${userId}`,
+      {
         bvn: data.bvn,
         image: data.image
-    },
-        {
-            withCredentials: true,
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + localStorage.getItem("token")
-            }
+      },
+      {
+        withCredentials: true,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token")
         }
+      }
     );
 
     const { success, message, isVerified } = response.data;
-    if(!success) {
+    if (!success) {
       toast.error(message);
       return;
     }
@@ -45,13 +46,20 @@ function BVNForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('bvn')} type="text" placeholder="BVN" required />
-      <input {...register('image')} type="file" accept=".jpg, .jpeg, .png" required />
-      <button type="submit">Submit</button>
-      {errors.bvn && <p>Please enter a valid BVN</p>}
-      {errors.image && <p>Please upload an image</p>}
-    </form>
+    <section className="fundwallet">
+      <form className="fund-form" onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("bvn")} type="text" placeholder="BVN" required />
+        <input
+          {...register("image")}
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          required
+        />
+        <button type="submit">Submit</button>
+        {errors.bvn && <p>Please enter a valid BVN</p>}
+        {errors.image && <p>Please upload an image</p>}
+      </form>
+    </section>
   );
 }
 

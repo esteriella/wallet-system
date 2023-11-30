c28925ace42a84c7a9926e45f9635e551295e426
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-11-30 08:11:42 +0100

    Update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -16,19 +16,45 @@ const getUser = async (req, res) => {
   }
 };
 
+// const updateUser = async (req, res) => {
+//   try {
+//     // Destructure the request body for cleaner code
+//     const { firstName, lastName, email } = req.body;
+
+//     // Create an object with the fields to update
+//     const updatedFields = {
+//       firstName,
+//       lastName,
+//       email,
+//     };
+
+//     // Update the user document and return the updated user
+//     const user = await User.findByIdAndUpdate(
+//       req.params.userId,
+//       updatedFields,
+//       { new: true }
+//     ).select("-password");
+
+//     if (!user) {
+//       // Handle the case where the user is not found
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     // Respond with the updated user data
+//     res.status(201).json({
+//       message: "Profile updated successfully",
+//       success: true
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal server error", success: true });
+//   }
+// };
+
 const updateUser = async (req, res) => {
   try {
-    // Destructure the request body for cleaner code
-    const { firstName, lastName, email } = req.body;
+    // ... (existing code)
 
-    // Create an object with the fields to update
-    const updatedFields = {
-      firstName,
-      lastName,
-      email,
-    };
-
-    // Update the user document and return the updated user
     const user = await User.findByIdAndUpdate(
       req.params.userId,
       updatedFields,
@@ -36,14 +62,74 @@ const updateUser = async (req, res) => {
     ).select("-password");
 
     if (!user) {
-      // Handle the case where the user is not found
-      return res.status(404).json({ message: "User not found" });
+      return res.status(400).json({ message: "User not found", success: false });
     }
 
-    // Respond with the updated user data
+    // ... (existing code)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", success: false });
+  }
+};
+
+
+// const verifyBVN = async (req, res) => {
+//   try{
+//     const { bvn } = req.body;
+//     const { image } = req.file;
+//     const user = await User.findById(req.params.userId);
+//     if (!user) {
+//       return res.status(404).json({ message: "Something went wrong, try again later", success: true });
+//     }
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.params.userId,
+//       { 
+//         bvn, 
+//         image,
+//         isVerified: true
+//       }, 
+//       { new: true }
+//     );
+//     const isVerified = updatedUser.isVerified;
+//     if (!isVerified) {
+//       return res.status(404).json({ message: "Something went wrong, try again later", success: false });
+//     }
+//     res.status(201).json({
+//       message: "BVN verified successfully",
+//       success: true,
+//       isVerified
+//     });
+//   }
+//   catch(error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Something went wrong, try again later", success: false });
+//   }
+// }
+
+const verifyBVN = async (req, res) => {
+  try {
+    const { bvn } = req.body;
+    const { image } = req.file;
+    const user = await User.findById(req.params.userId);
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found", success: false });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      { bvn, image, isVerified: true },
+      { new: true }
+    );
+
+    if (!updatedUser.isVerified) {
+      return res.status(500).json({ message: "BVN verification failed", success: false });
+    }
+
     res.status(201).json({
-      message: "Profile updated successfully",
-      success: true
+      message: "BVN verified successfully",
+      success: true,
+      isVerified: updatedUser.isVerified
     });
   } catch (error) {
     console.error(error);
@@ -51,38 +137,6 @@ const updateUser = async (req, res) => {
   }
 };
 
-const verifyBVN = async (req, res) => {
-  try{
-    const { bvn } = req.body;
-    const { image } = req.file;
-    const user = await User.findById(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ message: "Something went wrong, try again later", success: false });
-    }
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.userId,
-      { 
-        bvn, 
-        image,
-        isVerified: true
-      }, 
-      { new: true }
-    );
-    const isVerified = updatedUser.isVerified;
-    if (!isVerified) {
-      return res.status(404).json({ message: "Something went wrong, try again later", success: false });
-    }
-    res.status(201).json({
-      message: "BVN verified successfully",
-      success: true,
-      isVerified
-    });
-  }
-  catch(error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong, try again later", success: false });
-  }
-}
 
 const updatePassword = async (req, res) => {
   try {

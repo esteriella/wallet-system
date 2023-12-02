be64bfda15bb29785e8c49691576a4a1d6afa700
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-12-02 08:37:09 +0100

    Update

--- backend/utils/Secrets.js
+++ backend/utils/Secrets.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const secretTime = process.env.SECRET_TIME;
+const secretTime = process.env.SECRETS_TIME;
 
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
